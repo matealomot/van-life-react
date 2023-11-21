1a0faef1f0fc1a427d18ce23a7ba3e666a61f24c
matealomot <miss.mitic@protonmail.com>
2023-11-21 01:06:27 +0100

    Edited the errorElement using useActionData()

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,9 @@
-import { useLoaderData, Form } from "react-router-dom";
+import { useLoaderData, Form, useActionData } from "react-router-dom";
 import { useState } from "react";
 import { loginUser } from "../utilities/UtilityFunctions";
 import {redirect} from '../utilities/addOnUtilities'
 import '../css/Login.css';
+
 
 export function loader({ request }) {
     return new URL(request.url).searchParams.get("message")
@@ -10,30 +11,36 @@ export function loader({ request }) {
 
 export async function action({request}) {
     
-    if(!JSON.parse(localStorage.getItem('loggedin'))) {
-        const formData = await request.formData();
-        const email = formData.get('email');
-        const password = formData.get('password');
-        const data = await loginUser({email, password});
-        localStorage.setItem('loggedin', true)
-        
-        return redirect('/host');
+    try {
+        if(!JSON.parse(localStorage.getItem('loggedin'))) {
+            const formData = await request.formData();
+            const email = formData.get('email');
+            const password = formData.get('password');
+            const data = await loginUser({email, password});
+            localStorage.setItem('loggedin', true)
+            
+            return redirect('/host');
+        }
+        else {
+            return redirect("/login?message=You are already signed in.");
+        }
     }
-    else {
-        throw redirect("/login?message=You are already signed in.");
+    catch(err) {
+        return err.message
     }
 };
 
 export default function Login() {
     const [status, setStatus] = useState('idle');
-    const [errorState, setErrorState] = useState(null);
     const message = useLoaderData();
+    const error = useActionData();
+
 
     return (
         <div className="login-container">
             <h1>Sign in to your account</h1>
             {message && <h2 className="red">{message}</h2>}
-            {errorState && <h2 className="red">{errorState.message}</h2>}
+            {error && <h2 className="red">{error}</h2>}
             <Form method="post" className="login-form" replace>
                 <input
                     name="email"
